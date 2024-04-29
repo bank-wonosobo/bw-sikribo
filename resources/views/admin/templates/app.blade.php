8f45bb0b100195d8d73cb0a45eed64b08c7e7219
Ahmad Rifai <ahmmd.riffai@gmail.com>
2024-04-29 10:18:36 +0700

    fix:add link profile and photo

--- resources/views/admin/templates/app.blade.php
+++ resources/views/admin/templates/app.blade.php
@@ -83,12 +83,12 @@
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ AuthSSO::user()->name }}</span>
                                 <img class="img-profile rounded-circle"
-                                    src="img/undraw_profile.svg">
+                                    src="{{ asset('assets/admin/img/undraw_profile.svg') }}">
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="#">
+                                <a class="dropdown-item" href="https://account.bankwonosobo.co.id">
                                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Profile
                                 </a>
