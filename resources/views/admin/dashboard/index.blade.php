2a20172875aa29cb25f8007b4f1f4b3ec2aacf6d
unknown <ahmdriffai>
2024-05-02 14:24:34 +0700

    change:ubah dashboard dan tambah logo

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,97 +1,144 @@
 @extends('admin.templates.app')
 @section('title', 'Dashboard')
 @section('content')
+<section class="section dashboard">
+
 <div class="row mb-3">
     <div class="col-12">
-        <div class="row d-flex justify-content-center align-items-center flex-column bg-white">
-            <img src="{{ asset('/assets/admin/img/bg-01.png') }}" class="img-fluid rounded" width="350px" alt="image-profile">
-            <h5 class="font-weight-bold text-gray-900 mt-3">Selamat datang, {{ AuthSSO::user()->name }}</h5>
-            <p>Manage your archive to make Banking work better for you.</p>
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="row d-flex justify-content-center align-items-center flex-column bg-white">
+                            <img src="{{ asset('/assets/admin/img/bg-01.png') }}" class="img-fluid rounded"  alt="image-profile">
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <h5 class="mt-3 card-title">Selamat datang, {{ AuthSSO::user()->name }}</h5>
+                        <p>Manage your archive to make Banking work better for you,  and happy working :)</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
-<div clasxs="row">
-    <!-- Earnings (Monthly) Card Example -->
-    <div class="col-xl-3 col-md-6 mb-4">
-        <div class="card border-left-primary shadow h-100 py-2">
-            <div class="card-body">
-                <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                            Arsip Perjanjian Kredit</div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ App\Models\Kredit::count() }} Arsip</div>
-                    </div>
-                    <div class="col-auto">
-                        <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="row">
+<!-- Right side columns -->
+<div class="col-lg-4">
 
-    {{-- <!-- Earnings (Monthly) Card Example -->
-    <div class="col-xl-3 col-md-6 mb-4">
-        <div class="card border-left-success shadow h-100 py-2">
-            <div class="card-body">
-                <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                            Earnings (Annual)</div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
-                    </div>
-                    <div class="col-auto">
-                        <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <!-- News & Updates Traffic -->
+    <div class="card">
+      <div class="filter">
+        <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
+        <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+          <li class="dropdown-header text-start">
+            <h6>Filter</h6>
+          </li>
 
-    <!-- Earnings (Monthly) Card Example -->
-    <div class="col-xl-3 col-md-6 mb-4">
-        <div class="card border-left-info shadow h-100 py-2">
-            <div class="card-body">
-                <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
-                        </div>
-                        <div class="row no-gutters align-items-center">
-                            <div class="col-auto">
-                                <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
-                            </div>
-                            <div class="col">
-                                <div class="progress progress-sm mr-2">
-                                    <div class="progress-bar bg-info" role="progressbar"
-                                        style="width: 50%" aria-valuenow="50" aria-valuemin="0"
-                                        aria-valuemax="100"></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-auto">
-                        <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+          <li><a class="dropdown-item" href="#">Today</a></li>
+          <li><a class="dropdown-item" href="#">This Month</a></li>
+          <li><a class="dropdown-item" href="#">This Year</a></li>
+        </ul>
+      </div>
 
-    <!-- Pending Requests Card Example -->
-    <div class="col-xl-3 col-md-6 mb-4">
-        <div class="card border-left-warning shadow h-100 py-2">
-            <div class="card-body">
-                <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                            Pending Requests</div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
-                    </div>
-                    <div class="col-auto">
-                        <i class="fas fa-comments fa-2x text-gray-300"></i>
-                    </div>
+      <div class="card-body pb-0">
+        <h5 class="card-title">News &amp; Updates <span>| Today</span></h5>
+
+        <div class="news">
+          <div class="post-item clearfix">
+            <img src="{{ asset('templates/assets/img/news-1.jpg') }}" alt="">
+            <h4><a href="#">Nihil blanditiis at in nihil autem</a></h4>
+            <p>Sit recusandae non aspernatur laboriosam. Quia enim eligendi sed ut harum...</p>
+          </div>
+
+          <div class="post-item clearfix">
+            <img src="{{ asset('templates/assets/img/news-2.jpg') }}" alt="">
+            <h4><a href="#">Quidem autem et impedit</a></h4>
+            <p>Illo nemo neque maiores vitae officiis cum eum turos elan dries werona nande...</p>
+          </div>
+
+        </div><!-- End sidebar recent posts-->
+
+      </div>
+    </div><!-- End News & Updates -->
+
+  </div><!-- End Right side columns -->
+      <!-- Left side columns -->
+      <div class="col-lg-8">
+        <div class="row">
+
+          <!-- Sales Card -->
+          <div class="col-xxl-4 col-md-6">
+            <div class="card info-card sales-card">
+
+              <div class="filter">
+                <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
+                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                  <li class="dropdown-header text-start">
+                    <h6>Filter</h6>
+                  </li>
+
+                  <li><a class="dropdown-item" href="#">Today</a></li>
+                  <li><a class="dropdown-item" href="#">This Month</a></li>
+                  <li><a class="dropdown-item" href="#">This Year</a></li>
+                </ul>
+              </div>
+
+              <div class="card-body">
+                <h5 class="card-title">Arsip <span>| Today</span></h5>
+
+                <div class="d-flex align-items-center">
+                  <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                    <i class="bi bi-cart"></i>
+                  </div>
+                  <div class="ps-3">
+                    <h6>{{ App\Models\Kredit::count() }} Arsip</h6>
+                    {{-- <span class="text-success small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">increase</span> --}}
+
+                  </div>
                 </div>
+              </div>
+
             </div>
+          </div><!-- End Sales Card -->
+
+          <!-- Revenue Card -->
+          <div class="col-xxl-4 col-md-6">
+            <div class="card info-card revenue-card">
+
+              <div class="filter">
+                <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
+                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                  <li class="dropdown-header text-start">
+                    <h6>Filter</h6>
+                  </li>
+
+                  <li><a class="dropdown-item" href="#">Today</a></li>
+                  <li><a class="dropdown-item" href="#">This Month</a></li>
+                  <li><a class="dropdown-item" href="#">This Year</a></li>
+                </ul>
+              </div>
+
+              <div class="card-body">
+                <h5 class="card-title">Slik <span>| This Month</span></h5>
+
+                <div class="d-flex align-items-center">
+                  <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                    <i class="bi bi-currency-dollar"></i>
+                  </div>
+                  <div class="ps-3">
+                    <h6>{{ App\Models\Slik::count() }} Slik</h6>
+                    {{-- <span class="text-success small pt-1 fw-bold">8%</span> <span class="text-muted small pt-2 ps-1">increase</span> --}}
+
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div><!-- End Revenue Card -->
+
         </div>
-    </div> --}}
-</div>
+      </div><!-- End Left side columns -->
+    </div>
+</section>
 @endsection
