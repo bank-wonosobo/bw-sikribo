1c8bddf7062b33f3e81d825bf16cd7113805a201
unknown <ahmdriffai>
2024-05-02 14:32:12 +0700

    change:add favicon

--- resources/views/admin/templates/app.blade.php
+++ resources/views/admin/templates/app.blade.php
@@ -10,8 +10,8 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="{{ asset('templates/assets/img/favicon.png') }}" rel="icon">
-  <link href="{{ asset('templates/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+  <link href="{{ asset('logo.png') }}" rel="icon">
+  <link href="{{ asset('logo.png') }}" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
